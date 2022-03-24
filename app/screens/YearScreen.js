960d1f6b71abcea69ea855dfe3fcdca78b7b03ad
Shreyas Hondadakatti <shreyashondadakatti@airasia.com>
2022-03-24 16:58:32 +0530

    [FEAT] Adapt for 4 papers of mains in previous years' papers

--- app/screens/YearScreen.js
+++ app/screens/YearScreen.js
@@ -7,11 +7,18 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import Modal from 'react-native-modal';
 
 import colors from '../config/colors';
 import {headerTitleCreater} from '../navigation/AppNavigator';
 
-const OptionButton = ({item, index, params}) => {
+const OptionButton = ({
+  item,
+  index,
+  params,
+  setMainsPapers,
+  setShowMainsPapers,
+}) => {
   const [showOptions, setShowOptions] = useState(false);
   return showOptions ? (
     <View style={styles.topic}>
@@ -39,7 +46,8 @@ const OptionButton = ({item, index, params}) => {
           width: '50%',
         }}
         onPress={() => {
-          Linking.openURL(params.quizzes[item].mains);
+          setShowMainsPapers(true);
+          setMainsPapers(params.quizzes[item].mains);
         }}>
         <Text key={index} style={styles.text}>
           Mains
@@ -65,12 +73,22 @@ const OptionButton = ({item, index, params}) => {
 
 const YearScreen = ({route, navigation}) => {
   const {params} = route;
+  const [showMainsPapers, setShowMainsPapers] = useState(false);
+  const [mainsPapers, setMainsPapers] = useState({});
   navigation.setOptions({
     headerTitle: headerTitleCreater(params.name.toUpperCase()),
   });
 
   const renderItem = ({item, index}) => {
-    return <OptionButton item={item} index={index} params={params} />;
+    return (
+      <OptionButton
+        item={item}
+        index={index}
+        params={params}
+        setMainsPapers={setMainsPapers}
+        setShowMainsPapers={setShowMainsPapers}
+      />
+    );
   };
   return (
     <View style={styles.container}>
@@ -82,6 +100,36 @@ const YearScreen = ({route, navigation}) => {
         renderItem={renderItem}
         style={styles.flatlist}
       />
+      <Modal
+        isVisible={showMainsPapers}
+        onBackButtonPress={() => setShowMainsPapers(false)}
+        onBackdropPress={() => setShowMainsPapers(false)}>
+        <View style={styles.mainsPapers}>
+          {Object.keys(mainsPapers)
+            .sort((a, b) => -1)
+            .map((paper, index) => {
+              return (
+                <TouchableOpacity
+                  onPress={() => {
+                    setShowMainsPapers(false);
+                    Linking.openURL(mainsPapers[paper]);
+                  }}
+                  style={[
+                    styles.paper,
+                    index % 2 !== 0 ? styles.evenPaper : {},
+                  ]}>
+                  <Text
+                    style={[
+                      styles.paperText,
+                      index % 2 !== 0 ? styles.evenPaperText : {},
+                    ]}>
+                    {'Paper ' + (index + 1)}
+                  </Text>
+                </TouchableOpacity>
+              );
+            })}
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -96,6 +144,35 @@ const styles = StyleSheet.create({
     marginVertical: 16,
     width: '100%',
   },
+  mainsPapers: {
+    alignItems: 'center',
+    backgroundColor: colors.white,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+    paddingVertical: 16,
+  },
+  paper: {
+    alignItems: 'center',
+    backgroundColor: colors.primary,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    width: '100%',
+    marginVertical: 8,
+  },
+  paperText: {
+    color: colors.white,
+    fontSize: 18,
+  },
+  evenPaper: {
+    backgroundColor: colors.white,
+    borderWidth: 2,
+    borderColor: colors.yellow,
+  },
+  evenPaperText: {
+    color: colors.yellow,
+  },
+
   pdf: {
     flex: 1,
   },
