1f595121bc25a3bf471dcf2e4c196574e6fabca4
Shreyas Hondadakatti <shreyashondadakatti@airasia.com>
2022-04-15 00:32:56 +0530

    [FIX] Prelims test time

--- app/screens/PrelimsTestSeriesScreen.js
+++ app/screens/PrelimsTestSeriesScreen.js
@@ -12,7 +12,7 @@ import {ms, vs} from '../utils/scalingUtils';
 
 const PrelimsTestSeriesScreen = ({route, navigation}) => {
   const {params} = route;
-  const testTime = 10;
+  const testTime = parseInt(params.quizzes.testTime || 2) * 60 * 60;
   const getQuiz = (quiz) => {
     if (quiz.length === undefined) {
       quiz = Object.values(quiz);
